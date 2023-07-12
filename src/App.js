1afb0c6647bd6fbb532a2fad7d180b608a3ac5f7
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-07-12 12:20:43 -0700

    added click functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import { useState } from "react";
 import "./App.css";
-import Square from "./components/Square";
+import Square from "./Square";
 
 function App() {
-  const [squares, setSquares] = useState(['','','','','','','','',''])
-  const [player, setPlayer] = useState(true)
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
+
+  const handleClick = (square) => {
+    setSquares(["", "", "", "", "", "", "", "", '']);
+    setPlayer(true);
+  };
   return (
-    <div className="app" >
-      <div className="container" >
-        {squares.map((val, index)=>{
-          return <Square
-          squares={squares}
-          setSquares={setSquares}
-          player={player}
-          setPlayer={setPlayer}
-          squareValue={val}
-          index = {index}
-          />
+    <div className="App">
+      <button onClick={handleClick}>Reset</button>
+      <div className="container">
+        {squares.map((val, index) => {
+          // console.log('sending', <Square/>)
+          return (
+            <Square
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+              squareValue={val}
+              index={index}
+            />
+          );
         })}
       </div>
     </div>
---
+++ src/Square.js
@@ -0,0 +1,24 @@
+const Square = (props) => {
+  // console.log(props.squares, props.player);
+
+  const handleClick = () => {
+    console.log('handling click')
+    if (!props.squareValue) {
+      if(props.player){
+        props.squares.splice(props.index, 1 , 'X')
+        props.setPlayer(!props.player)
+      }else {
+        props.squares.splice(props.index, 1, 'O')
+        props.setPlayer(!props.player)
+      }
+    };
+  }
+
+  return <div className="square" onClick={handleClick}>
+    {props.SquareValue === 'O'? 'O': props.squareValue}
+    
+  </div>;
+};
+
+
+export default Square;
